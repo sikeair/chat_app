d9f9c9cd0315278c00b4b4e0db167a6fa7672952
SikE <asinayo@gmail.com>
2023-02-28 04:51:05 +0900

    chat_app_24ing

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:chat_app/screen/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginSignupScreen(),
+    );
+  }
+}
---
+++ lib/screen/chat_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggedUser;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _authentication.currentUser;
+      if (user != null) {
+        loggedUser = user;
+        print(loggedUser!.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _authentication.signOut();
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.exit_to_app_sharp,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Chat screen'),
+      ),
+    );
+  }
+}
