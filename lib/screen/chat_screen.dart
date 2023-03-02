c18746a199ca81e2939d6c4638c5665d74e797ac
SikE <asinayo@gmail.com>
2023-03-03 00:42:44 +0900

    chat_app_chapter25

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -49,8 +50,31 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Chat screen'),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/dwpnZ3ICNA81avcE9xoQ/message')
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  docs[index]['text'],
+                  style: const TextStyle(fontSize: 20.0),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
